d8d1dd91c06364887c94efc020bcd175c08b178a
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2021-10-12 15:22:12 +0800

    Hapus yg tdk digunakan di modul Group Pengguna

--- resources/views/role/edit.blade.php
+++ resources/views/role/edit.blade.php
@@ -1,16 +1,16 @@
-@extends( 'layouts.dashboard_template' )
+@extends('layouts.dashboard_template')
 
-@section('title') Create Role @endsection
-
-@section( 'content' )
+@section('content')
+<!-- Content Header (Page header) -->
 <section class="content-header">
 	<h1>
-	Role Management
+		{{ $page_title ?? "Page Title" }}
+		<small>{{ $page_description ?? '' }}</small>
 	</h1>
 	<ol class="breadcrumb">
-		<li><a href="#"><i class="fa fa-dashboard"></i> Administrator</a></li>
-		<li><a href="#">Roles</a></li>
-		<li class="active">Edit</li>
+		<li><a href="{{route('dashboard')}}"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+		<li><a href="{{route('setting.role.index')}}">{{ $page_title }}</a></li>
+		<li class="active">{{ $page_description }}</li>
 	</ol>
 </section>
 
