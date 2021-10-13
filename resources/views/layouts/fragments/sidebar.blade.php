e909306b092aa35290e82a46f33ba613d8a43237
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2021-10-13 15:34:06 +0800

    Seragamkan icon sidebar

--- resources/views/layouts/fragments/sidebar.blade.php
+++ resources/views/layouts/fragments/sidebar.blade.php
@@ -68,7 +68,7 @@ $user = Sentinel::getUser();
                         @endif
                         @if($user->hasAnyAccess(['admin', 'data-penduduk']))
                             <li class="treeview {{ (Request::is(['data/penduduk/*', 'data/penduduk/index', 'data/penduduk', 'data/keluarga/*', 'data/keluarga/index', 'data/keluarga', 'data/laporan-penduduk'])? 'active' : '') }}">
-                                <a href="#"><i class="fa fa-users"></i>Kependudukan
+                                <a href="#"><i class="fa fa-circle-o"></i>Kependudukan
                                     <span class="pull-right-container">
                                         <i class="fa fa-angle-left pull-right"></i>
                                     </span>
