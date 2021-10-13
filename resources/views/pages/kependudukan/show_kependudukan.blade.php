e2c7e93d3734f7be2a52b2f0705c010befecd342
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2021-10-13 18:04:45 +0800

    Error profil_id

--- resources/views/pages/kependudukan/show_kependudukan.blade.php
+++ resources/views/pages/kependudukan/show_kependudukan.blade.php
@@ -10,7 +10,6 @@
                     <div class="form-group">
                         <label for="list_desa" class="col-sm-4 control-label">Desa</label>
                         <div class="col-sm-8">
-                            <input type="hidden" id="profil_id" value="{{ $profil_id }}">
                             <select class="form-control" id="list_desa">
                                 <option value="Semua">Semua Desa</option>
                                 @foreach($list_desa as $desa)
