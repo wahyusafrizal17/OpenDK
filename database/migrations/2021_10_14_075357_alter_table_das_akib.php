e2d3fc36eb5899a87eb9b3578a25b208eeb69123
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2021-10-14 09:09:10 +0800

    Fix seeder dan impor akib

---
+++ database/migrations/2021_10_14_075357_alter_table_das_akib.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTableDasAkib extends Migration
+{
+    public function up()
+    {
+        Schema::table('das_akib', function($table) {
+            $table->dropColumn('kecamatan_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('das_akib', function (Blueprint $table) {
+            $table->char('kecamatan_id', 8);
+        });
+    }
+}
